b37fa0ddc0b8425d0848685ba5a27d2fa1f163d1
adi <adanut@hiberus.com>
2022-05-12 13:04:25 +0200

    a

--- TiendaPC/src/app/app.module.ts
+++ TiendaPC/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './Components/productos/productos.component';
+import { UsuariosComponent } from './Components/usuarios/usuarios.component';
+import { CategoriasComponent } from './Components/categorias/categorias.component';
+import { PedidosComponent } from './Components/pedidos/pedidos.component';
+import { DetallePedidosComponent } from './Components/detalle-pedidos/detalle-pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    UsuariosComponent,
+    CategoriasComponent,
+    PedidosComponent,
+    DetallePedidosComponent
   ],
   imports: [
     BrowserModule,
